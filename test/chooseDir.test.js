05a6381213066714c2e7a8cfa1f31eff41c70090
Arniu <arniu2006@gmail.com>
2019-01-10 12:31:42 +0800

    Add more tests for `chooseDir`

--- test/chooseDir.test.js
+++ test/chooseDir.test.js
@@ -1,21 +1,32 @@
 const t = require('tap')
+const fs = require('fs')
 
 const chooseDir = require('../lib/chooseDir')
 
-t.test('choose template dir', t => {
-  t.plan(2)
+t.test('choose template dir to generate an app', t => {
+  t.plan(3)
 
-  t.match(
-    chooseDir({
-      plugin: false
-    }),
-    /app$/
-  )
+  const dir = chooseDir({
+    plugin: false
+  })
 
-  t.match(
-    chooseDir({
-      plugin: true
-    }),
-    /plugin$/
-  )
+  t.match(dir, /app$/)
+  fs.stat(dir, (err, stat) => {
+    t.error(err)
+    t.ok(stat.isDirectory())
+  })
+})
+
+t.test('choose template dir to generate a plugin', t => {
+  t.plan(3)
+
+  const dir = chooseDir({
+    plugin: true
+  })
+
+  t.match(dir, /plugin$/)
+  fs.stat(dir, (err, stat) => {
+    t.error(err)
+    t.ok(stat.isDirectory())
+  })
 })
