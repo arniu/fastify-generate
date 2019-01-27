46bcc01da685cbec16b658e439055518eb00bf4f
Arniu <arniu2006@gmail.com>
2019-01-27 13:29:47 +0800

    Extract main

--- bin/cli.js
+++ bin/cli.js
@@ -2,33 +2,7 @@
 
 'use strict'
 
-const help = require('../lib/help')
-const version = require('../lib/version')
-const generate = require('../lib/generate')
-const parseArgs = require('../lib/parseArgs')
-const log = require('../lib/log')
-
-function main (args) {
-  const options = parseArgs(args)
-
-  if (options.help) {
-    help()
-  } else if (options.version) {
-    version()
-  } else if (options._.length === 1) {
-    generate(options)
-  } else {
-    help()
-  }
-}
-
-// Handle errors centrally
-// https://github.com/i0natan/nodebestpractices#2-error-handling-practices
-process.on('uncaughtException', err => {
-  process.exitCode = 1
-
-  log.error(err)
-})
+const main = require('../lib/main')
 
 if (require.main === module) {
   main(process.argv.slice(2))
